f998b22c50d4159b14202d1a967ec1f875988e49
Jose Fernandez <jose@crosbyinteractive.com>
2020-07-28 02:28:53 -0400

    Fixed a build issue with vue-kinesis.

--- src/components/Hero.vue
+++ src/components/Hero.vue
@@ -1,70 +1,72 @@
 <template>
-    <kinesis-container>
-        <section class="hero-area bg-primary">
-            <div class="container">
-                <div class="row">
-                    <div class="col-lg-11 mx-auto">
-                        <h1 class="text-white font-tertiary">Hi! I’m <br> Jose Fernandez<br> Software Engineer</h1>
+    <ClientOnly>
+        <kinesis-container>
+            <section class="hero-area bg-primary">
+                <div class="container">
+                    <div class="row">
+                        <div class="col-lg-11 mx-auto">
+                            <h1 class="text-white font-tertiary">Hi! I’m <br> Jose Fernandez<br> Software Engineer</h1>
+                        </div>
                     </div>
                 </div>
-            </div>
-            <div class="layer-bg w-100">
-                <g-image class="img-fluid w-100" src="~/assets/images/illustrations/leaf-bg.png" alt="bg-shape" />
-            </div>
-            <div class="layer" id="l2">
-                <kinesis-element :strength="20">
-                    <g-image src="~/assets/images/illustrations/dots-cyan.png" alt="bg-shape" />
-                </kinesis-element>
-            </div>
-            <div class="layer" id="l3">
-                <kinesis-element :strength="20">
-                    <g-image src="~/assets/images/illustrations/leaf-orange.png" alt="bg-shape" />
-                </kinesis-element>
-            </div>
-            <div class="layer" id="l4">
-                <kinesis-element :strength="20">
-                    <g-image src="~/assets/images/illustrations/dots-orange.png" alt="bg-shape" />
-                </kinesis-element>
-            </div>
-            <div class="layer" id="l5">
-                <kinesis-element :strength="20">
-                    <g-image src="~/assets/images/illustrations/leaf-yellow.png" alt="bg-shape" />
-                </kinesis-element>
-            </div>
-            <div class="layer" id="l6">
-                <kinesis-element :strength="20">
-                    <g-image src="~/assets/images/illustrations/leaf-cyan.png" alt="bg-shape" />
-                </kinesis-element>
-            </div>
-            <div class="layer" id="l7">
-                <kinesis-element :strength="20">
-                    <g-image src="~/assets/images/illustrations/dots-group-orange.png" alt="bg-shape" />
-                </kinesis-element>
-            </div>
-            <div class="layer" id="l8">
-                <kinesis-element :strength="20">
-                    <g-image src="~/assets/images/illustrations/leaf-pink-round.png" alt="bg-shape" />
-                </kinesis-element>
-            </div>
-            <div class="layer" id="l9">
-                <kinesis-element :strength="20">
-                    <g-image src="~/assets/images/illustrations/leaf-cyan-2.png" alt="bg-shape" />
-                </kinesis-element>
-            </div>
-            <ul class="list-unstyled ml-5 mt-3 position-relative zindex-1">
-                <li class="mb-3">
-                    <a class="text-white" href="https://github.com/jfern01" target="_blank">
-                        <ThemifyIcon icon="github" />
-                    </a>
-                </li>
-                <li class="mb-3">
-                    <a class="text-white" href="https://www.linkedin.com/in/jose-fernandez-89179540/" target="_blank">
-                        <ThemifyIcon icon="linkedin" />
-                    </a>
-                </li>
-            </ul>
-        </section>
-    </kinesis-container>
+                <div class="layer-bg w-100">
+                    <g-image class="img-fluid w-100" src="~/assets/images/illustrations/leaf-bg.png" alt="bg-shape" />
+                </div>
+                <div class="layer" id="l2">
+                    <kinesis-element :strength="20">
+                        <g-image src="~/assets/images/illustrations/dots-cyan.png" alt="bg-shape" />
+                    </kinesis-element>
+                </div>
+                <div class="layer" id="l3">
+                    <kinesis-element :strength="20">
+                        <g-image src="~/assets/images/illustrations/leaf-orange.png" alt="bg-shape" />
+                    </kinesis-element>
+                </div>
+                <div class="layer" id="l4">
+                    <kinesis-element :strength="20">
+                        <g-image src="~/assets/images/illustrations/dots-orange.png" alt="bg-shape" />
+                    </kinesis-element>
+                </div>
+                <div class="layer" id="l5">
+                    <kinesis-element :strength="20">
+                        <g-image src="~/assets/images/illustrations/leaf-yellow.png" alt="bg-shape" />
+                    </kinesis-element>
+                </div>
+                <div class="layer" id="l6">
+                    <kinesis-element :strength="20">
+                        <g-image src="~/assets/images/illustrations/leaf-cyan.png" alt="bg-shape" />
+                    </kinesis-element>
+                </div>
+                <div class="layer" id="l7">
+                    <kinesis-element :strength="20">
+                        <g-image src="~/assets/images/illustrations/dots-group-orange.png" alt="bg-shape" />
+                    </kinesis-element>
+                </div>
+                <div class="layer" id="l8">
+                    <kinesis-element :strength="20">
+                        <g-image src="~/assets/images/illustrations/leaf-pink-round.png" alt="bg-shape" />
+                    </kinesis-element>
+                </div>
+                <div class="layer" id="l9">
+                    <kinesis-element :strength="20">
+                        <g-image src="~/assets/images/illustrations/leaf-cyan-2.png" alt="bg-shape" />
+                    </kinesis-element>
+                </div>
+                <ul class="list-unstyled ml-5 mt-3 position-relative zindex-1">
+                    <li class="mb-3">
+                        <a class="text-white" href="https://github.com/jfern01" target="_blank">
+                            <ThemifyIcon icon="github" />
+                        </a>
+                    </li>
+                    <li class="mb-3">
+                        <a class="text-white" href="https://www.linkedin.com/in/jose-fernandez-89179540/" target="_blank">
+                            <ThemifyIcon icon="linkedin" />
+                        </a>
+                    </li>
+                </ul>
+            </section>
+        </kinesis-container>
+    </ClientOnly>
 </template>
 
 <script>
