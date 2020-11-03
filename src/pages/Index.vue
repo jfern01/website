8e9ce8c1398c3083c65dd1856d0cdb67e98769c1
Jose Fernandez <jose.fernandez@kygunco.com>
2020-11-03 07:57:11 -0500

    Updated a link on the homepage.

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -9,7 +9,7 @@
                     <div class="col-lg-10 mx-auto text-center">
                         <p class="font-secondary paragraph-lg text-dark">Jose <span class="text-primary">♥</span> Open Source</p>
                         <p class="font-secondary paragraph-lg text-dark">A graduate of ITT Tech, Jose Fernandez has been with Crosby Interactive since 2013. He enjoys challenging projects that provide opportunities for problem solving and learning new skills. Jose specializes in providing software solutions which may include Dev Ops, Linux systems administration and data analysis. His favorite programming languages are Python, TypeScript, PHP and Go. Jose prefers the freedom and innovation of open source technologies and is always looking for new ways to leverage and contribute to the free and open source software community.</p>
-                        <g-link to="/about/" class="btn btn-transparent">know more</g-link>
+                        <g-link to="https://www.linkedin.com/in/jose-fernandez-89179540/" target="_blank" class="btn btn-transparent">know more</g-link>
                     </div>
                 </div>
             </div>
